bf390b8d143077003fcbca34b559f91722b50e05
Aleksander Kocaj <alekskocaj@googlemail>
2023-09-27 09:18:12 +0000

    add connection to git hub

--- git_setup.R
+++ git_setup.R
@@ -6,3 +6,13 @@ usethis::use_git_config(
   init.defaultBranch = "main") # <-- not necessary but kinder than 'master'
 
 usethis::use_git()
+
+usethis::create_github_token(description = "Token for Repro Workshop 2023 Test")
+
+# my personal token for git hub:  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 
+
+gitcreds::gitcreds_set() # <-- Token must *not* go into brackets, paste when asked
+
+usethis::gh_token_help() # Verify that everything is in order
+
+usethis::use_github() # To activate GitHub and upload your files to the public web
